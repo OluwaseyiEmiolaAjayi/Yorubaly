dee5c91ed9fe4c0b691bb15f5f8e2537287a8bc4
Oluwaseyi Ajayi <oluwaseyieajayi@gmail.com>
2023-12-12 17:04:03 +0100

    install react js and tailwind, create first template for Yorubaly Estate

---
+++ script.js
@@ -0,0 +1,42 @@
+// // let a=10
+// // function outer() {
+// //     let b=20
+// //     function inner() {
+// //         let c=30
+
+// //         console.log(a, b, c)
+// //     }
+// //     inner()
+// // }
+// // outer()
+
+// function outer() {
+//     let counter = 0
+
+//     function inner() {
+//         counter++
+//         console.log(counter)
+//     }
+//     return inner
+// }
+// const fn = outer()
+// fn()
+// fn()
+
+function sum(a, b, c) {
+    return a+b+c
+
+
+function curry(fn) {
+    return function(a) {
+        return function(b) {
+            return function(c) {
+                return fn(a, b, c)
+            }
+        }
+    }
+}
+
+const curriedSum = curry(sum)
+console.log(curriedSum(2)(3)(5))
+
