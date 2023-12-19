8b39a0dcb8e477c6204b31eddad1574af28c471a
Oluwaseyi Ajayi <oluwaseyieajayi@gmail.com>
2023-12-19 23:48:09 +0100

    YorubalyTOWN- connect to datatbase

---
+++ yorubaly_estate/client/src/components/Header.jsx
@@ -0,0 +1,39 @@
+import {FaSearch} from 'react-icons/fa';
+import {Link} from 'react-router-dom';
+
+export default function Header() {
+  return (
+    <header style={{backgroundColor:'#020035'
+        }} className='shadow-md'> 
+       <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
+        
+        <Link to='/'>
+        <h1 className='text-sm sm:text-xl flex flex-wrap'>
+          <span style={{ color: 'white' }}>YorubalY</span>
+          <span className='text-white'>STATE</span>
+        </h1>
+        </Link>
+
+        <form style={{backgroundColor:'#fff'
+        }} className='rounded-lg p-3 flex items-center'>
+          <input type="text" placeholder='Search' className='bg-transparent focus:outline-none w-24 sm:w-64 md:w-32' />
+          <FaSearch style={{color: '#000'}}/>
+        </form>
+
+        <ul className='flex gap-4 '>
+          <Link to='/'>
+          <li className='hidden sm:inline text-white hover:underline'>Home</li>
+          </Link>
+
+          <Link to='/about'>
+          <li className='hidden sm:inline text-white hover:underline'>About</li>
+          </Link>
+
+          <Link to='/sign-in'>
+          <li className='text-white hover:underline'>Sign in</li>
+          </Link>
+        </ul>
+       </div>
+    </header>
+  )
+}
